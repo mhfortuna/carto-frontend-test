d7157c8de9595c4bc9da4ccd986455ef1d8236f5
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-11-11 08:34:17 +0100

    feat: project initialization

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import MapContainer from "./Components/MapContainer/MapContainer";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        {/* <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header> */}
+        <MapContainer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MapContainer/MapContainer.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { StaticMap, MapContext, NavigationControl } from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+// import {ColumnLayer} from '@deck.gl/layers';
+
+// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
+const AIR_PORTS =
+  "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson";
+
+const INITIAL_VIEW_STATE = {
+  latitude: 51.47,
+  longitude: 0.45,
+  zoom: 4,
+  bearing: 0,
+  pitch: 30,
+};
+
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json";
+const NAV_CONTROL_STYLE = {
+  position: "absolute",
+  top: 10,
+  left: 10,
+};
+
+export default function MapContainer() {
+  const onClick = (info) => {
+    if (info.object) {
+      // eslint-disable-next-line
+      alert(
+        `${info.object.properties.name} (${info.object.properties.abbrev})`,
+      );
+    }
+  };
+
+  const layers = [
+    new GeoJsonLayer({
+      id: "airports",
+      data: AIR_PORTS,
+      // Styles
+      filled: true,
+      pointRadiusMinPixels: 2,
+      pointRadiusScale: 2000,
+      getPointRadius: (f) => 11 - f.properties.scalerank,
+      getFillColor: [200, 0, 80, 180],
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      onClick,
+    }),
+    // new ArcLayer({
+    //   id: "arcs",
+    //   data: AIR_PORTS,
+    //   dataTransform: (d) =>
+    //     d.features.filter((f) => f.properties.scalerank < 4),
+    //   // Styles
+    //   getSourcePosition: () => [-0.4531566, 51.4709959], // London
+    //   getTargetPosition: (f) => f.geometry.coordinates,
+    //   getSourceColor: [0, 128, 200],
+    //   getTargetColor: [200, 0, 80],
+    //   getWidth: 1,
+    // }),
+  ];
+
+  return (
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller
+      layers={layers}
+      ContextProvider={MapContext.Provider}
+    >
+      <StaticMap mapStyle={MAP_STYLE} />
+      <NavigationControl style={NAV_CONTROL_STYLE} />
+    </DeckGL>
+  );
+}
