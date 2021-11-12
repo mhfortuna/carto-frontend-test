5760083043bfbb1a57098517853eb195bb4248e8
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-11-12 14:51:31 +0100

    feat: added classes for negative z-index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        "-10": "-10",
+        "-20": "-20",
+      },
+    },
   },
   variants: {
     extend: {},
